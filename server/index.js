6fb9596ec774e381f13a61c714d31a448d367c2e
Zachary Kremer <zachary.kremer@genesis-seating.com>
2021-10-14 13:59:36 -0400

    Server added

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+
+
+const port = 5000;
+
+//Builds Server
+const app = express();
+const clientPath = `${__dirname}/../client`;
+app.use(cors());
+app.use(express.static(clientPath));
+const server = http.createServer(app);
+
+//Starts Server
+server.on('error', (err) => {
+    console.error('Server error:', err);
+});
+  
+server.listen(port, () => {
+    console.log(`app started at http://localhost:${port}`);
+});
