c291b1c297bb39a390fbfef25e7cbb966fa44ef4
Zachary Kremer <zachary.kremer@genesis-seating.com>
2021-10-19 15:25:26 -0400

    Modal Spacing Fixed

--- client/src/checkout.js
+++ client/src/checkout.js
@@ -13,11 +13,8 @@ export const Checkout = class Checkout {
         })));
     
         lInnerDiv.appendChild(simpleElement("h1", aItemName));
-        lInnerDiv.appendChild(document.createElement("br"));
-        lInnerDiv.appendChild(document.createElement("br"));
 
         let lDescription = document.createElement("div");
-
         for(let lChild of simpleElement("div", null, {innerHTML: aItemDescription}).children) {
             if (lChild.tagName.includes("H") && lChild.tagName.length == 2) {
                 lDescription.appendChild(document.createElement("br"));
